6f2556d9e9111b1e7d7fcd56ae5668ffbe5f9754
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2026-05-13 13:23:56 +0300

    checking

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,44 @@
+import { defineConfig } from 'vite';
 import viteImagemin from 'vite-plugin-imagemin';
 
-export default () => {
-  return {
-    plugins: [
-      viteImagemin({
-        gifsicle: {
-          optimizationLevel: 3,
-          interlaced: false,
-        },
-        optipng: {
-          optimizationLevel: 3,
-        },
-        mozjpeg: {
-          quality: 80,
-        },
-        pngquant: {
-          quality: [0.8, 0.9],
-          speed: 4,
-        },
-        svgo: {
-          plugins: [
-            {
-              name: 'removeViewBox',
-            },
-            {
-              name: 'removeEmptyAttrs',
-              active: false,
-            },
-          ],
-        },
-      }),
-    ],
-  };
-};
+export default defineConfig({
+  plugins: [
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+
+      optipng: {
+        optimizationLevel: 7,
+      },
+
+      mozjpeg: {
+        quality: 80,
+        progressive: false,
+      },
+
+      pngquant: {
+        quality: [0.75, 0.9],
+        speed: 3,
+      },
+
+      webp: {
+        quality: 80,
+      },
+
+      avif: {
+        quality: 60,
+      },
+
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
+});
