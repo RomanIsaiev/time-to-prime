cd6f40edfdaea57ebc6d551bae3538c07244b33d
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2026-05-13 13:39:14 +0300

    upd swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.lectors-swiper', {
   speed: 400,
   spaceBetween: 20,
   slidesPerView: 'auto',
@@ -8,15 +8,12 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-const reviewsSwiper = new Swiper(
-  '.reviews-swiper',
-  {
-    speed: 400,
-    spaceBetween: 20,
-    slidesPerView: 1,
-    navigation: {
-      nextEl: '.review-button-next',
-      prevEl: '.review-button-prev',
-    },
-  }
-);
+const reviewsSwiper = new Swiper('.reviews-swiper', {
+  speed: 400,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.review-button-next',
+    prevEl: '.review-button-prev',
+  },
+});
